be82196c68e1abc7353a26c27cb3d997b3dc0887
chchico <c28hero@gmail.com>
2019-10-07 16:32:44 +0900

    ReactTestを追加（コピペ）

---
+++ WebApplication1/WebApplication1/Controllers/ReactTest01Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ReactTest01Controller : Controller
+    {
+        // GET: ReactTest01
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
